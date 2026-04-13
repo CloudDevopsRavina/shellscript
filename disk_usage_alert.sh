c90edca204056f2de2c8fa1d460ae1a0dc6695af
Ravina Rathod <ravinarathod009@gmail.com>
2026-04-13 13:51:33 +0530

    Update disk_usage_alert.sh

--- disk_usage_alert.sh
+++ disk_usage_alert.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+THRESHOLD=80
+
+df -h | awk 'NR>1 {print $5 " " $6}' | while read output;
+do
+    usage=$(echo $output | awk '{print $1}' | sed 's/%//g')
+    partition=$(echo $output | awk '{print $2}')
+
+    if [ $usage -ge $THRESHOLD ]; then
+        echo "WARNING: $partition is ${usage}% full"
+    fi
+done
